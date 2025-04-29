261cf4aa9fc91f399210c6e8a408f71122d13a00
im-karesz <karesz@iparimarketing.hu>
2025-04-29 10:49:52 +0200

    Delete contact-info-mask.php

--- contact-info-mask.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Elérhetőségek elrejtése és felfedése funkció
-
-function im_contact_info_mask_enqueue_scripts() {
-    wp_enqueue_script('im-contact-info-mask', plugin_dir_url(__FILE__) . 'contact-info-mask.js', array('jquery'), '1.0', true);
-    wp_enqueue_style('im-contact-info-mask', plugin_dir_url(__FILE__) . 'contact-info-mask.css');
-}
-add_action('wp_enqueue_scripts', 'im_contact_info_mask_enqueue_scripts');
